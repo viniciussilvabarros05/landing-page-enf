77b4716187811e13773cb69b43c8800a7817cb9b
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2024-10-01 18:45:46 -0300

    feat: add list dynamic of categories

--- src/components/profiles.tsx
+++ src/components/profiles.tsx
@@ -5,15 +5,34 @@ import {
   CarouselItem,
   CarouselPrevious,
   CarouselNext,
+  type CarouselApi,
 } from "./ui/carousel";
 import { Container } from "./ui/container";
 import WordPullUp from "./ui/word-pull-up";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useInView } from "framer-motion";
 import BlurFade from "./ui/blur-fade";
+import { categories } from "@/utils/data";
 export const Profiles = () => {
   const ref = useRef(null);
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
   const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
+
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
+
   return (
     <Container>
       <div ref={ref}>
@@ -25,21 +44,36 @@ export const Profiles = () => {
         )}
       </div>
       <BlurFade duration={0.3} delay={0.3} inView>
-        <Carousel className="mt-40">
+        <Carousel className="mt-20" opts={{ loop: true }} setApi={setApi}>
           <CarouselContent>
-            <CarouselItem className="">
-              <div className="border-blue-500 border-2 size-72 rounded-full m-auto"></div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="border-blue-500 border-2 size-72 rounded-full m-auto"></div>
-            </CarouselItem>
-            <CarouselItem>
-              <div className="border-blue-500 border-2 size-72 rounded-full m-auto"></div>
-            </CarouselItem>
+            {categories.map((category) => {
+              return (
+                <CarouselItem className="basis-1/3 flex items-center justify-center flex-col gap-3">
+                  <div className="border-blue-500 border-2 size-56 rounded-full m-auto overflow-hidden p-4">
+                    <img src={category.img} className="w-full h-full object-contain"/>
+                  </div>
+                  <p className="text-md text-blue-500 font-semibold capitalize">{category.label}</p>
+                </CarouselItem>
+              );
+            })}
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />
         </Carousel>
+
+        <div className="my-8 flex gap-2 mx-auto w-fit">
+          {Array.from({ length: count }, (_, index) => index).map(
+            (point, index) => {
+              return (
+                <div
+                  className={`size-2 rounded-full transition-all duration-300 ${
+                    index + 1 == current ? "bg-yellow-400" : "bg-gray-400"
+                  }`}
+                ></div>
+              );
+            }
+          )}
+        </div>
       </BlurFade>
     </Container>
   );
