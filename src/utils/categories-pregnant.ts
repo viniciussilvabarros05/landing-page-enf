c4b5331da556f5fb96747eaf2cdb33d1e7f77243
Vinicius Silva Barros <viniciussilvabarros05@hotmail.com>
2024-11-23 11:22:33 -0300

    feat: add images of categories pregnants

--- src/utils/categories-pregnant.ts
+++ src/utils/categories-pregnant.ts
@@ -1,17 +1,17 @@
 export const categoriesPregnant = [
     {
         id: 300,
-        img: "/images/",
+        img: "/images/Puerpera.png",
         label: "Específicas para puérperas"
     },
     {
         id: 310,
-        img: "/images/",
+        img: "/images/Gestante_Puerpera.png",
         label: "Gestante e Puérpera a qualquer momento"
     },
     {
         id: 320,
-        img: "/images/",
+        img: "/images/Gestante.png",
         label: "Gestante a partir da 20ª semana"
     },
 
